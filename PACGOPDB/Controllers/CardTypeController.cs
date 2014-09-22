b6cd8e1d579c38e43ae4f436fb588da7783d94ab
Mike Yacullo <mike.yacullo@gmail.com>
2014-09-22 07:45:43 -0400

    db initializer working

---
+++ PACGOPDB/Controllers/CardTypeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PACGOPDB.Models;
+using PACGOPDB.DAL;
+
+namespace PACGOPDB.Controllers
+{
+    public class CardTypeController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: /CardType/
+        public ActionResult Index()
+        {
+            return View(db.CardTypes.ToList());
+        }
+
+        // GET: /CardType/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CardType cardtype = db.CardTypes.Find(id);
+            if (cardtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cardtype);
+        }
+
+        // GET: /CardType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /CardType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Name")] CardType cardtype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CardTypes.Add(cardtype);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cardtype);
+        }
+
+        // GET: /CardType/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CardType cardtype = db.CardTypes.Find(id);
+            if (cardtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cardtype);
+        }
+
+        // POST: /CardType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Name")] CardType cardtype)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cardtype).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cardtype);
+        }
+
+        // GET: /CardType/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CardType cardtype = db.CardTypes.Find(id);
+            if (cardtype == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cardtype);
+        }
+
+        // POST: /CardType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CardType cardtype = db.CardTypes.Find(id);
+            db.CardTypes.Remove(cardtype);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- PACGOPDB/DAL/DBInitializer.cs
+++ PACGOPDB/DAL/DBInitializer.cs
@@ -7,27 +7,45 @@ using PACGOPDB.Models;
 
 namespace PACGOPDB.DAL
 {
-    public class DBInitializer : System.Data.Entity.CreateDatabaseIfNotExists<DBContext>
+    public class DBInitializer : System.Data.Entity.DropCreateDatabaseAlways<DBContext>
     {
         protected override void Seed(DBContext context)
         {
+            var seasons = new List<Season>{
+                new Season{ID=1,Name="Season of the Shackles"}
+            };
+            seasons.ForEach(s => context.Seasons.Add(s));
+            context.SaveChanges();
+
             var classDecks = new List<ClassDeck>{
-                new ClassDeck{SeasonID=0,Name="Bard"},
-                new ClassDeck{SeasonID=0,Name="Cleric"},
-                new ClassDeck{SeasonID=0,Name="Fighter"},
-                new ClassDeck{SeasonID=0,Name="Ranger"},
-                new ClassDeck{SeasonID=0,Name="Rogue"},
-                new ClassDeck{SeasonID=0,Name="Sorcerer"},
-                new ClassDeck{SeasonID=0,Name="Wizard"}
+                new ClassDeck{SeasonID=1,Name="Bard"},
+                new ClassDeck{SeasonID=1,Name="Cleric"},
+                new ClassDeck{SeasonID=1,Name="Fighter"},
+                new ClassDeck{SeasonID=1,Name="Ranger"},
+                new ClassDeck{SeasonID=1,Name="Rogue"},
+                new ClassDeck{SeasonID=1,Name="Sorcerer"},
+                new ClassDeck{SeasonID=1,Name="Wizard"}
             };
             classDecks.ForEach(s => context.ClassDecks.Add(s));
             context.SaveChanges();
 
-            var seasons = new List<Season>{
-                new Season{ID=0,Name="Season of the Shackles"}
+            var cardTypes = new List<CardType>{
+                new CardType{Name="Weapon"},
+                new CardType{Name="Spell"},
+                new CardType{Name="Armor"},
+                new CardType{Name="Item"},
+                new CardType{Name="Ally"},
+                new CardType{Name="Blessing"}
             };
-            seasons.ForEach(s => context.Seasons.Add(s));
+            cardTypes.ForEach(s=>context.CardTypes.Add(s));
             context.SaveChanges();
+
+            var cardTraits = new List<CardTrait>{
+                new CardTrait{Name="Basic"}
+            };
+            cardTraits.ForEach(s => context.CardTraits.Add(s));
+            context.SaveChanges();
+            
         }
     }
 }
